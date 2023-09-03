a6ab3fd84f6075a523fa80dfd9c1a87cb49e8d2c
timseymour42 <73562010+timseymour42@users.noreply.github.com>
2023-09-03 18:02:00 -0400

    filling out the about me page

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,12 @@
 import React, { Component } from "react";
 import { Icon } from "@iconify/react";
-import angularIcon from "@iconify/icons-logos/angular-icon";
+import pythonIcon from "@iconify/icons-logos/python";
 import reactIcon from "@iconify/icons-logos/react";
-import vueIcon from "@iconify/icons-logos/vue";
+import javaIcon from "@iconify/icons-logos/java";
+import mysqlIcon from "@iconify/icons-logos/mysql";
+import javascriptIcon from "@iconify/icons-logos/javascript";
+// import typescriptIcon from "@iconify/icons-logos/skill-icons:typescript";
+
 
 class About extends Component {
   render() {
@@ -18,8 +22,8 @@ class About extends Component {
     return (
       <section id="about">
         <div className="col-md-12">
-          <h1 style={{ color: "black" }}>
-            <span>{sectionName}</span>
+          <h1 style={{ color: "black", fontSize: "300%" }}>
+            <span>{sectionName}</span> About Me
           </h1>
           <div className="row center mx-auto mb-5">
             <div className="col-md-4 mb-5 center">
@@ -28,20 +32,34 @@ class About extends Component {
                   <img
                     height="250px"
                     src={profilepic}
-                    alt="Avatar placeholder"
+                    alt="Skills"
                   />
+                  <div className="icon-grid">
                   <Icon
-                    icon={angularIcon}
-                    style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
+                    icon={pythonIcon}
+                    style={{ fontSize: "600%", margin: "4% 2% 0 2%" }}
                   />
                   <Icon
                     icon={reactIcon}
-                    style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
+                    style={{ fontSize: "600%", margin: "4% 2% 0 2%" }}
                   />
                   <Icon
-                    icon={vueIcon}
-                    style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
+                    icon={javaIcon}
+                    style={{ fontSize: "600%", margin: "4% 2% 0 2%" }}
                   />
+                  <Icon
+                    icon={mysqlIcon}
+                    style={{ fontSize: "600%", margin: "4% 2% 0 2%" }}
+                  />
+                  <Icon
+                    icon={javascriptIcon}
+                    style={{ fontSize: "600%", margin: "4% 2% 0 2%" }}
+                  />
+                   {/* <Icon
+                    icon={typescriptIcon}
+                    style={{ fontSize: "600%", margin: "4% 2% 0 2%" }}
+                  /> */}
+                  </div>
                 </span>
               </div>
             </div>
@@ -72,15 +90,24 @@ class About extends Component {
                     className="card-body font-trebuchet text-justify ml-3 mr-3"
                     style={{
                       height: "auto",
-                      fontSize: "132%",
+                      fontSize: "180%",
                       lineHeight: "200%",
                     }}
                   >
                     <br />
-                    <span className="wave">{hello} :) </span>
+                    <span className="wave">{hello} Hey there </span>
                     <br />
                     <br />
-                    {about}
+                    {about} 👋 I'm Tim. I am a data scienstist and a sports analytics nerd. The Yankees <img src={`${process.env.PUBLIC_URL}/images/team-logos/yankees.png`} 
+                                                                                                            alt="Yankees logo" 
+                                                                                                            style={{ width: '20px', height: '20px' }} />
+                                                                                                            , Patriots <img src={`${process.env.PUBLIC_URL}/images/team-logos/patriots.png`} 
+                                                                                                            alt="Patriots logo" 
+                                                                                                            style={{ width: '30px', height: '30px' }} />, 
+                                                                                                            and UConn basketball <img src={`${process.env.PUBLIC_URL}/images/team-logos/uconn.png`} 
+                                                                                                            alt="UConn logo" 
+                                                                                                            style={{ width: '21px', height: '25px' }} /> are my favorite teams. 
+                                                                                                            I got my start in data science because I love doing sports analytics projects to investigate trends in sports and uncover strategic advantages for coaching or the front office.
                   </div>
                 </div>
               </div>
