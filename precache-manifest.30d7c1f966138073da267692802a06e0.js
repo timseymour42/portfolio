f2bdfdc2ca2249656538ca3e7d9d3b44bec91faf
timseymour42 <73562010+timseymour42@users.noreply.github.com>
2023-09-08 16:10:55 -0400

    Updates

---
+++ precache-manifest.30d7c1f966138073da267692802a06e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93e19e65d19c199e2ef684738c3646cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3eca1f9157a4b9030c4",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "8c0f76b3f1aa6f7b7598",
+    "url": "/static/css/main.35ef0362.chunk.css"
+  },
+  {
+    "revision": "d3eca1f9157a4b9030c4",
+    "url": "/static/js/2.7a32c06e.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.7a32c06e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c0f76b3f1aa6f7b7598",
+    "url": "/static/js/main.8b20c5e8.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-frontend-dev-portfolio/precache-manifest.232be26275fdcd76b7d0348ca2237f4a.js"
+  "/precache-manifest.30d7c1f966138073da267692802a06e0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-frontend-dev-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
